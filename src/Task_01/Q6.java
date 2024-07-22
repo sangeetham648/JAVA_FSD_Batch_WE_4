3697ec678fe48a70691b863b2dcb4a98001a7e39
Sangeetha <sangeetham648@gmail.com>
2024-07-22 13:33:18 +0530

    Task 1 Submission
    Task 1

--- src/Task_01/Q6.java
+++ src/Task_01/Q6.java
@@ -11,9 +11,9 @@ public class Q6 {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if (j < i) {
-                    System.out.print(n - j + "j ");
+                    System.out.print(n - j + " ");
                 } else {
-                    System.out.print(n - i + "i ");
+                    System.out.print(n - i + " ");
                 }
             }
             System.out.println();
@@ -22,3 +22,27 @@ public class Q6 {
         sc.close();
     }
 }
+//Output:
+//Enter number : 5
+//        5 5 5 5 5
+//        5 4 4 4 4
+//        5 4 3 3 3
+//        5 4 3 2 2
+//        5 4 3 2 1
+
+//Enter number : 3
+//        3 3 3
+//        3 2 2
+//        3 2 1
+
+//Enter number : 10
+//        10 10 10 10 10 10 10 10 10 10
+//        10 9 9 9 9 9 9 9 9 9
+//        10 9 8 8 8 8 8 8 8 8
+//        10 9 8 7 7 7 7 7 7 7
+//        10 9 8 7 6 6 6 6 6 6
+//        10 9 8 7 6 5 5 5 5 5
+//        10 9 8 7 6 5 4 4 4 4
+//        10 9 8 7 6 5 4 3 3 3
+//        10 9 8 7 6 5 4 3 2 2
+//        10 9 8 7 6 5 4 3 2 1
